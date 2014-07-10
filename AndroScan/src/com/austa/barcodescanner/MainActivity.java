ab078ef2078d5e1a4694024fe7887c4de63fc8d4
austa <alaattin.usta@bil.omu.edu.tr>
2014-07-10 17:06:41 +0300

    bug-fixed

--- AndroScan/src/com/austa/barcodescanner/MainActivity.java
+++ AndroScan/src/com/austa/barcodescanner/MainActivity.java
@@ -1,16 +1,17 @@
 package com.austa.barcodescanner;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.json.JSONObject;
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 import com.android.volley.Request;
 import com.android.volley.Request.Method;
@@ -19,28 +20,37 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.austa.barcodescanner.gson.parse.JsonParseClass;
+import com.austa.barcodescanner.adapter.ImageSearchResultAdapter;
+import com.austa.barcodescanner.gson.parse.ProductContainerClass;
 import com.austa.barcodescanner.gson.parse.ProductPropertyClass;
 import com.austa.barcodescanner.utils.Alerts;
+import com.google.gson.Gson;
 
 public class MainActivity extends Activity {
-    private TextView tvResult;
-    private JsonParseClass jtos;
-    private RequestQueue mRequestQueue;
-    private Button btnIkınci;
-    private static List<ProductPropertyClass> productList = new ArrayList<ProductPropertyClass>();
+    private String url = "http://www.searchupc.com/handlers/upcsearch.ashx?request_type=3&access_token=C1D63810-388B-4B3E-BECD-5778741E60E0&upc=";
+    private Button scanButton;
+    private ListView resultList;
+    private ProgressBar progressBar;
+    private RequestQueue queue;
+    private Gson gson;
+    private JsonObjectRequest searchRequest, extendedRequest;
+    private ImageSearchResultAdapter adapter;
+    private List<ProductPropertyClass> productList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        btnIkınci = (Button) findViewById(R.id.btnIkinci);
-        tvResult = (TextView) findViewById(R.id.tvResult);
-
-        Button scanBtn = (Button) findViewById(R.id.btnScan);
-        scanBtn.setOnClickListener(new OnClickListener() {
+        setContentView(R.layout.second_activity);
+        scanButton = (Button) findViewById(R.id.scanButton);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        resultList = (ListView) findViewById(R.id.resultsList);
+        queue = Volley.newRequestQueue(getApplicationContext());
+        scanButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                scanButton.setEnabled(false);
+                progressBar.setVisibility(View.VISIBLE);
+                resultList.setVisibility(View.GONE);
                 try {
                     Intent intent = new Intent("com.google.zxing.client.android.SCAN");
                     intent.putExtra("SCAN_MODE", "QR_CODE_MODE,PRODUCT_MODE");
@@ -51,16 +61,6 @@ public class MainActivity extends Activity {
                 }
             }
         });
-        btnIkınci.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
-                startActivity(intent);
-
-            }
-        });
-
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
@@ -68,38 +68,28 @@ public class MainActivity extends Activity {
             if (resultCode == RESULT_OK) {
                 String result = intent.getStringExtra("SCAN_RESULT");
                 generateRequest(result);
-                tvResult.setText(editJsonEntity(productList.toString()));
-            } else if (resultCode == RESULT_CANCELED) {
-                tvResult.setText("Sonuç dönmedi.");
-            }
+            } else if (resultCode == RESULT_CANCELED) {}
         }
     }
 
     private void generateRequest(String result) {
-        String url = "http://www.searchupc.com/handlers/upcsearch.ashx?request_type=3&access_token=C1D63810-388B-4B3E-BECD-5778741E60E0&upc="
-                + result;
-
-        jtos = new JsonParseClass();
-
-        final ProgressDialog pDialog = new ProgressDialog(this);
-        pDialog.setMessage("Bilgiler geliyor...");
-        pDialog.show();
-        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Method.GET, url, null, new Response.Listener<JSONObject>() {
+        String newUrl = url + result;
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Method.GET, newUrl, null, new Response.Listener<JSONObject>() {
 
             @Override
             public void onResponse(JSONObject response) {
-                pDialog.hide();
-                productList = jtos.addCurrentProductToProductsListUsingGson(response);
+                scanButton.setEnabled(true);
+                setUpResults(response);
             }
         }, new Response.ErrorListener() {
 
             @Override
             public void onErrorResponse(VolleyError error) {
-                pDialog.hide();
+                scanButton.setEnabled(true);
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(getApplicationContext(), "Error, Please Try Again", Toast.LENGTH_LONG).show();
             }
         });
-
-        // Adding request to request queue
         addToRequestQueue(jsonObjReq);
     }
 
@@ -109,11 +99,11 @@ public class MainActivity extends Activity {
     }
 
     public RequestQueue getRequestQueue() {
-        if (mRequestQueue == null) {
-            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        if (queue == null) {
+            queue = Volley.newRequestQueue(getApplicationContext());
         }
 
-        return mRequestQueue;
+        return queue;
     }
 
     public <T> void addToRequestQueue(Request<T> req) {
@@ -124,4 +114,54 @@ public class MainActivity extends Activity {
         jsonEntity = jsonEntity.substring(1, jsonEntity.length() - 1);
         return jsonEntity;
     }
+
+    private void setUpResults(JSONObject response) {
+
+        productList = new ArrayList<ProductPropertyClass>();
+        Gson g = new Gson();
+        ProductContainerClass vc = g.fromJson(findAndReplace(response), ProductContainerClass.class);
+        productList = vc.getProduct();
+        adapter = new ImageSearchResultAdapter(this, productList);
+        resultList.setAdapter(adapter);
+        resultList.setVisibility(View.VISIBLE);
+        //get the next 8 results in the list           
+        progressBar.setVisibility(View.GONE);
+
+    }
+
+    private void addExtendedResults(JSONObject response) {
+        ProductPropertyClass searchClass = gson.fromJson(response.toString(), ProductPropertyClass.class);
+        if (searchClass.getResponse() != null) {
+            ProductPropertyClass[] results = searchClass.getResponse().getResults();
+            List<ProductPropertyClass> tempList = Arrays.asList(results);
+            productList.addAll(tempList);
+            resultList.setAdapter(adapter);
+        }
+    }
+
+    public String findAndReplace(JSONObject currentProduct) {
+        char theLastCharacter;
+        char theFirstCharacter;
+        final String PRODUCT_NAME = "\"product\":[";
+        final String THE_LAST_CHARACTER = "]";
+        int counter = 0;
+        String notInNumberJsonObjectString, newString = null;
+        notInNumberJsonObjectString = currentProduct.toString();
+
+        while(counter < 5) {
+            String mystring = "\"" + counter + "\":";
+            if (notInNumberJsonObjectString.contains(mystring)) {
+                notInNumberJsonObjectString = notInNumberJsonObjectString.replace(mystring, "");
+            }
+            counter++;
+        }
+
+        theFirstCharacter = notInNumberJsonObjectString.charAt(0);
+        theLastCharacter = notInNumberJsonObjectString.charAt(notInNumberJsonObjectString.length() - 1);
+        notInNumberJsonObjectString = notInNumberJsonObjectString.substring(1, notInNumberJsonObjectString.length() - 1);
+
+        newString = theFirstCharacter + PRODUCT_NAME + notInNumberJsonObjectString + THE_LAST_CHARACTER + theLastCharacter;
+        return newString;
+    }
+
 }
--- AndroScan/src/com/austa/barcodescanner/SplashActivity.java
+++ AndroScan/src/com/austa/barcodescanner/SplashActivity.java
@@ -1,65 +1,63 @@
 package com.austa.barcodescanner;
 
-
-
-import com.austa.barcodescanner.utils.UiViewHelper;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import com.austa.barcodescanner.utils.UiViewHelper;
 
 public class SplashActivity extends Activity {
-	private View mViewMain;
+    private View mViewMain;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_splash);
-		setUI();
-		startTimer();
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        setUI();
+        startTimer();
+    }
 
-	private void setUI() {
-		
-		mViewMain = (View) findViewById(R.id.vw_splash_main);
+    private void setUI() {
 
-	}
+        mViewMain = (View) findViewById(R.id.vw_splash_main);
 
-	private void startTimer() {
-		
-		new Handler().postDelayed(new Runnable() {
+    }
 
-			@Override
-			public void run() {
-				finishActivity();
-			}
+    private void startTimer() {
 
-		}, 2500);
+        new Handler().postDelayed(new Runnable() {
 
-	}
-	/**
-	 * finish activity without memory expand
-	 */
-	private void finishActivity() {
-	
-		SplashActivity.this.finish();
-		Intent intent = new Intent();
-		intent.setClass(this, MainActivity.class);
-		intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-		startActivity(intent);
+            @Override
+            public void run() {
+                finishActivity();
+            }
 
-	}
+        }, 2500);
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-	}
+    }
 
-	@Override
-	protected void onDestroy() {
-		UiViewHelper.unbindDrawables(mViewMain);
-		super.onDestroy();
-	}
+    /**
+     * finish activity without memory expand
+     */
+    private void finishActivity() {
+
+        SplashActivity.this.finish();
+        Intent intent = new Intent();
+        intent.setClass(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(intent);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        UiViewHelper.unbindDrawables(mViewMain);
+        super.onDestroy();
+    }
 }
